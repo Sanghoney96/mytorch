59fe814b3bc0ee168ca1d123fa6e15fe5a8fa45f
Jeongsangheon <shultra2@gmail.com>
2023-04-10 22:38:39 +0900

    put column_stack instead of vstack

--- fc.py
+++ fc.py
@@ -1,17 +1,16 @@
 import numpy as np
 
 class Flatten:
-    def tensor2vector(self, matrices):
-        vector = np.array([[]])
+    def __call__(self, matrices):
         self.matrix_shape = matrices.shape
+        vector = np.array([[]])
 
         for i in range(matrices.shape[0]):
             reshaped_matrix = np.reshape(
-                matrices[i], (1, self.matrix_shape[1] * self.matrix_shape[2] * self.matrix_shape[3]))
-            vector = np.vstack((vector, reshaped_matrix))
+                matrices[i], (1, self.matrix_shape[1]*self.matrix_shape[2]*self.matrix_shape[3]))
+            vector = np.column_stack((vector, reshaped_matrix))
         return vector
 
 class Linear:
 
 class Dropout:
-    
