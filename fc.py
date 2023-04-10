519f2ddb80eaf9b2d4ac7a61256cee5b6ff0453b
Jeongsangheon <shultra2@gmail.com>
2023-04-10 18:13:07 +0900

    create Flatten

--- fc.py
+++ fc.py
@@ -1,1 +1,17 @@
 import numpy as np
+
+class Flatten:
+    def tensor2vector(self, matrices):
+        vector = np.array([[]])
+        self.matrix_shape = matrices.shape
+
+        for i in range(matrices.shape[0]):
+            reshaped_matrix = np.reshape(
+                matrices[i], (1, self.matrix_shape[1] * self.matrix_shape[2] * self.matrix_shape[3]))
+            vector = np.vstack((vector, reshaped_matrix))
+        return vector
+
+class Linear:
+
+class Dropout:
+    
