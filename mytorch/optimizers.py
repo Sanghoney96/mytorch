57ed5b799ec2c90b1453efc6e8a99c9bf54a7ae2
Sanghoney96 <shultra2@gmail.com>
2023-07-10 15:55:29 +0900

    Adam optimizer added

--- mytorch/optimizers.py
+++ mytorch/optimizers.py
@@ -54,3 +54,37 @@ class MomentumSGD(Optimizer):
         v = self.vs[v_key]
         v = self.momentum * v - self.lr * param.grad.data
         param.data += v
+
+
+class Adam(Optimizer):
+    def __init__(self, alpha=0.001, beta1=0.9, beta2=0.999, eps=1e-8):
+        super().__init__()
+        self.alpha = alpha
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.ms = {}
+        self.vs = {}
+        self.iter = 0
+
+    def update(self, *args, **kwargs):
+        self.t += 1
+        super().update(*args, **kwargs)
+
+    def update_one(self, param):
+        key = id(param)
+        if key not in self.ms:
+            self.ms[key] = Parameter(np.zeros_like(param.data))
+            self.vs[key] = Parameter(np.zeros_like(param.data))
+
+        m, v = self.ms[key], self.vs[key]
+        beta1, beta2, eps = self.beta1, self.beta2, self.eps
+        grad = param.grad.data
+
+        m = beta1 * m + (1 - beta1) * grad
+        v = beta2 * v + (1 - beta2) * grad * grad
+
+        m_hat = m / (1 - beta1**self.t)
+        v_hat = v / (1 - beta2**self.t)
+
+        param.data -= self.alpha * m_hat / (np.sqrt(v_hat) + eps)
